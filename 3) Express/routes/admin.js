675977d9bfb70262373d35a92de823807325b2d4
amir-roshan <amirvalielahi@gmail.com>
2024-06-27 20:44:37 -0700

    Add helper function (unused) for root key-based file finding
    - Add CSS styling to client side and manage it through Node.js

--- 3) Express/routes/admin.js
+++ 3) Express/routes/admin.js
@@ -1,11 +1,14 @@
 import express from "express";
 
+// we can use the root directory as a base path for the views
+import rootDir from "../utils/path.js";
+
 // This router is like a mini express app that can be plugged into another express app.
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    res.sendFile("/views/add-product.html", { root: "." });
 });
 
 // /admin/add-product => POST
@@ -13,6 +16,5 @@ router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
-
 
 export default router;
--- 3) Express/routes/shop.js
+++ 3) Express/routes/shop.js
@@ -2,9 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
+
 // default route is '/'
 router.get('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
+    // res.sendFile(path.join(__dirname, '..', "views", "shop.html"));
+    // OR
+    res.sendFile("/views/shop.html", { root: "." });
 });
 
 export default router;
